b75d5c6f81fc8dd6c976bc90afd696ae84a3c58f
gustavonvp <gustavonvp@gmail.com>
2021-07-17 06:17:14 -0300

    Acessing controller bean with context.

---
+++ src/main/java/com/sfgdi/sfgdi/Application.java
@@ -0,0 +1,23 @@
+package com.sfgdi.sfgdi;
+
+import com.sfgdi.sfgdi.controller.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx =   SpringApplication.run(Application.class, args);
+
+        MyController myController = (MyController) ctx.getBean("myController");
+
+
+        String greeting = myController.sayHello();
+    
+        System.out.println(greeting);
+    }
+
+}
