30a0fb335fca9ac950708ae50cd1e40468cf0eec
gustavonvp <gustavonvp@gmail.com>
2021-07-18 16:58:10 -0300

    Adding Base Entity(model) Closes #11

--- src/main/java/com/sfgdi/sfgdi/model/Person.java
+++ src/main/java/com/sfgdi/sfgdi/model/Person.java
@@ -1,6 +1,6 @@
 package com.sfgdi.sfgdi.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- src/main/java/com/sfgdi/sfgdi/model/PetType.java
+++ src/main/java/com/sfgdi/sfgdi/model/PetType.java
@@ -1,6 +1,6 @@
 package com.sfgdi.sfgdi.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
