b585a8e74aeaba4d98ab188273397ef223c62f68
gustavonvp <gustavonvp@gmail.com>
2021-07-19 05:24:05 -0300

    Adding Singleton and Prototype. Closes #24

--- src/main/java/com/sfgdi/sfgdi/Application.java
+++ src/main/java/com/sfgdi/sfgdi/Application.java
@@ -1,6 +1,8 @@
 package com.sfgdi.sfgdi;
 
 import com.sfgdi.sfgdi.controller.*;
+import com.sfgdi.sfgdi.services.PrototypeBean;
+import com.sfgdi.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -44,8 +46,19 @@ public class Application {
         System.out.println("---------- Constructor");
 
         ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-
         System.out.println(constructorInjectedController.getGreeting());
+
+        System.out.println("---- Bean Scopes -------");
+        SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean1.getMyScope());
+        SingletonBean singleBean2 = ctx.getBean(SingletonBean.class);
+        System.out.println(singleBean2.getMyScope());
+
+
+        PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean1.getMyScope());
+        PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean2.getMyScope());
     }
 
 }
