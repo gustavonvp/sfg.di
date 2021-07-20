0c9c80aabef601141e7ac08081819aab62e450fa
gustavonvp <gustavonvp@gmail.com>
2021-07-20 06:09:42 -0300

    Adding PetType Service and Map Closes #45

---
+++ src/main/java/com/sfgdi/sfgdi/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package com.sfgdi.sfgdi.services;
+
+import com.sfgdi.sfgdi.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+}
---
+++ src/main/java/com/sfgdi/sfgdi/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.sfgdi.sfgdi.services.map;
+
+import com.sfgdi.sfgdi.model.PetType;
+import com.sfgdi.sfgdi.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+
+    @Override
+    public Set<PetType> findALl() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
