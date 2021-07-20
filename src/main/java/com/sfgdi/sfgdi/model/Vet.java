14790562483753d94c43aad8d0d7b758ee41c2c1
gustavonvp <gustavonvp@gmail.com>
2021-07-20 05:56:49 -0300

    Adding Speciality class. Closes #43

--- src/main/java/com/sfgdi/sfgdi/model/Vet.java
+++ src/main/java/com/sfgdi/sfgdi/model/Vet.java
@@ -1,4 +1,18 @@
 package com.sfgdi.sfgdi.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialitySet;
+
+    public Set<Speciality> getSpecialitySet() {
+        return specialitySet;
+    }
+
+    public void setSpecialitySet(Set<Speciality> specialitySet) {
+        this.specialitySet = specialitySet;
+    }
+
+
 }
