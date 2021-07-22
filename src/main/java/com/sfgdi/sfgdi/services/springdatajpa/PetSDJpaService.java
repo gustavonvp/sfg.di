0acf8b4e806fc2dcde076ccbd682a8effddc6e6d
gustavonvp <gustavonvp@gmail.com>
2021-07-22 05:52:39 -0300

    Adding JPA Service for Specialty . Closes #59

---
+++ src/main/java/com/sfgdi/sfgdi/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,49 @@
+package com.sfgdi.sfgdi.services.springdatajpa;
+
+import com.sfgdi.pets.PetService;
+import com.sfgdi.sfgdi.model.Pet;
+import com.sfgdi.sfgdi.repositories.PetRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public String getPetType() {
+        return null;
+    }
+
+    @Override
+    public Set<Pet> findALl() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
