a134e0d98cd2b0f1ebf367460bfff48f356cb54c
gustavonvp <gustavonvp@gmail.com>
2021-07-22 06:11:16 -0300

    Creating Visit JPA Class Service . Closes #60

---
+++ src/main/java/com/sfgdi/sfgdi/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.sfgdi.sfgdi.services;
+
+import com.sfgdi.sfgdi.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ src/main/java/com/sfgdi/sfgdi/services/map/VisitMapService.java
@@ -0,0 +1,51 @@
+package com.sfgdi.sfgdi.services.map;
+
+import com.sfgdi.sfgdi.model.Visit;
+import com.sfgdi.sfgdi.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    private final VisitService visitService;
+
+    public VisitMapService(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+    @Override
+    public Set<Visit> findALl() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+            || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+
+        }
+            return super.save(visit);
+
+
+    }
+
+    @Override
+    public void delete( Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+
+}
