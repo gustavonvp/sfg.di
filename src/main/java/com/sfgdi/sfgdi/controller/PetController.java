19d6dc210b16c627944ef425d807e9e4c51e4388
gustavonvp <gustavonvp@gmail.com>
2021-08-03 16:48:26 -0300

    Adding Pet Template for request's. Closes #78

--- src/main/java/com/sfgdi/sfgdi/controller/PetController.java
+++ src/main/java/com/sfgdi/sfgdi/controller/PetController.java
@@ -1,19 +1,57 @@
 package com.sfgdi.sfgdi.controller;
 
+import com.sfgdi.sfgdi.model.Owner;
+import com.sfgdi.sfgdi.model.PetType;
+import com.sfgdi.sfgdi.services.OwnerService;
 import com.sfgdi.sfgdi.services.PetService;
+import com.sfgdi.sfgdi.services.PetTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
 
 @Controller
+@RequestMapping("/owners/{ownerId}")
 public class PetController {
 
+    private static final String VIEWS_PET_CREATE_OR_UPDATE_FORM = "pets/createOrUpdateForm";
+
     private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public PetController(@Qualifier("petServiceMap") PetService petService) {
+    public PetController(@Qualifier("petServiceMap") PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
         this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
     }
 
+    @ModelAttribute("types")
+    public Collection<PetType> populatedPetTypes() {
+        return petTypeService.findALl();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+
     public String whichPetIsTheBest() { return petService.getPetType();}
+
+
+
+
 }
