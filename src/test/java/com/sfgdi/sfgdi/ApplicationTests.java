e425e198bd7535075771b86ce9b85e855fa95e9a
gustavonvp <gustavonvp@gmail.com>
2021-08-09 19:07:46 -0300

    Refactoring Test's

--- src/test/java/com/sfgdi/sfgdi/ApplicationTests.java
+++ src/test/java/com/sfgdi/sfgdi/ApplicationTests.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest
 class ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
+//
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
